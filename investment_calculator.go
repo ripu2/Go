7880506bf8c817612dbf51a15cdbb0566f8fdf2f
ripu2 <damanripu664@gmail.com>
2025-02-09 14:34:13 +0530

    added investmet calculator

---
+++ investment_calculator.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate float64 = 2.5
+	var investmentAmount, years, expectedReturnRate float64
+
+	fmt.Print("Enter initial investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Enter year: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Enter expected annual return rate (in percentage): ")
+	fmt.Scan(&expectedReturnRate)
+
+	var netReturn = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	var inflationAdjustedReturn = netReturn / (math.Pow(1+inflationRate/100, years))
+
+	fmt.Printf("Inflation-adjusted return: INR %.2f\n", inflationAdjustedReturn)
+}
--- pkg/mod/github.com/pressly/goose/v3@v3.7.0/cmd/goose/driver_postgres.go
+++
@@ -1,8 +0,0 @@
-//go:build !no_postgres
-// +build !no_postgres
-
-package main
-
-import (
-	_ "github.com/jackc/pgx/v4/stdlib"
-)
